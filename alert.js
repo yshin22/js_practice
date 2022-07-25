6a6bfb418a39e5dc8dce2bb445f74df667daccc7
Yeoungmin Shin <scarh54@gmail.com>
2022-07-24 22:44:14 -0400

    Reactive Navbar and Darkmode button

--- alert.js
+++ alert.js
@@ -1,3 +1,23 @@
+function loadScript(src) {
+    let script = document.createElement('script')
+    script.src = src
+    document.head.append(script)
+}
+
+function darkMode() {
+    var element = document.body;
+    var btn_mode = document.getElementById("btn1");
+    if (btn_mode.value=="light mode") {
+        btn_mode.value="dark mode"
+        btn_mode.style.backgroundImage = "url(../styles/crescent-moon.png)"
+    }
+    else {
+        btn_mode.value="light mode"
+        btn_mode.style.backgroundImage = "url(../styles/sun_symbol.png)"
+    }
+    element.classList.toggle("dark-mode");
+}
+
 /*Backtick: allow embedding variables and expressions
 
 let name = "John";
@@ -137,16 +157,7 @@ alert(a == b)
 
 alert(a.name)*/
 
-function loadScript(src) {
-    let script = document.createElement('script')
-    script.src = src
-    document.head.append(script)
-}
 
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
 
 
 
